f067a07917a28563a6974807a69b56e7e901fd31
Marko Berger <marko.berger@intellegens.hr>
2020-01-02 16:00:05 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, ChangeDetectorRef, ChangeDetectionStrategy, ApplicationRef} from '@angular/core';
+import { Observable } from 'rxjs';
+import { WeatherService } from './weather.service';
+import { Weather } from './weather-model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ing-wind';
+
+  weather: Weather;
+
+  constructor(private weatherService: WeatherService, private app: ApplicationRef) {
+  }
+
+  ngOnInit(){
+    // this.cd.markForCheck();
+    this.weatherService.getWeather().subscribe(
+      (data: Weather) => {
+        this.weather = data;
+        console.log(this.weather);
+        this.app.tick();
+      }
+    )
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'd4c12e9404aeb9063935ffe1f96de24c'
 };
 
 /*
